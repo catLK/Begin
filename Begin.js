0261c2d6ea88a7a0dae0005bc5fee02931749272
catLK <33770032+catLK@users.noreply.github.com>
2023-11-21 16:25:39 +0800

    Update Begin.js

--- Begin.js
+++ Begin.js
@@ -1,73 +1,55 @@
-// 获取画布和上下文
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-
-// 获取绘制和擦除按钮
-const drawButton = document.getElementById('drawButton');
-const eraseButton = document.getElementById('eraseButton');
+// ... 其他代码 ...
 
 let drawing = false;
 
-// 绘制模式
-drawButton.addEventListener('click', () => {
-    drawing = true;
-    canvas.style.cursor = 'crosshair';
-});
-
-// 擦除模式
-eraseButton.addEventListener('click', () => {
-    drawing = false;
-    canvas.style.cursor = 'default';
-});
-
-// 鼠标按下事件
-canvas.addEventListener('mousedown', startDrawing);
-canvas.addEventListener('touchstart', startDrawing);
-
-// 鼠标移动事件
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('touchmove', draw);
-
-// 鼠标释放事件
-canvas.addEventListener('mouseup', stopDrawing);
-canvas.addEventListener('touchend', stopDrawing);
-
-// 开始绘制
 function startDrawing(e) {
     if (drawing) {
-        e.preventDefault();
-        ctx.beginPath();
-        if (e.type === 'mousedown') {
-            ctx.moveTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
-        } else if (e.type === 'touchstart') {
-            ctx.moveTo(e.touches[0].clientX - canvas.getBoundingClientRect().left, e.touches[0].clientY - canvas.getBoundingClientRect().top);
-        }
+        return;
     }
+    
+    e.preventDefault();
+    drawing = true;
+    
+    ctx.beginPath();
+    
+    if (e.type === 'mousedown') {
+        ctx.moveTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
+    } else if (e.type === 'touchstart') {
+        ctx.moveTo(e.touches[0].clientX - canvas.getBoundingClientRect().left, e.touches[0].clientY - canvas.getBoundingClientRect().top);
+    }
+    
+    // 添加明显的边框样式
+    canvas.style.border = '2px solid #000';
 }
 
-// 绘制
 function draw(e) {
-    if (drawing) {
-        e.preventDefault();
-        if (e.type === 'mousemove') {
-            ctx.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
-        } else if (e.type === 'touchmove') {
-            ctx.lineTo(e.touches[0].clientX - canvas.getBoundingClientRect().left, e.touches[0].clientY - canvas.getBoundingClientRect().top);
-        }
-        ctx.stroke();
+    if (!drawing) {
+        return;
     }
+    
+    e.preventDefault();
+    
+    if (e.type === 'mousemove') {
+        ctx.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
+    } else if (e.type === 'touchmove') {
+        ctx.lineTo(e.touches[0].clientX - canvas.getBoundingClientRect().left, e.touches[0].clientY - canvas.getBoundingClientRect().top);
+    }
+    
+    // 使用requestAnimationFrame以便绘制更平滑
+    requestAnimationFrame(drawFrame);
 }
 
-// 停止绘制
+function drawFrame() {
+    ctx.stroke();
+}
+
 function stopDrawing() {
+    if (drawing) {
+        drawing = false;
+        canvas.style.border = '1px solid #000'; // 恢复默认边框样式
+    }
     canvas.removeEventListener('mousemove', draw);
     canvas.removeEventListener('touchmove', draw);
 }
 
-// 清空画布
-function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-// 清空按钮
-document.getElementById('clearButton').addEventListener('click', clearCanvas);
+// ... 其他代码 ...
