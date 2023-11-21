f1c12af2858313aea6bc06f23a811fc4c31fa63e
catLK <33770032+catLK@users.noreply.github.com>
2023-11-21 16:18:58 +0800

    Update Begin.js

--- Begin.js
+++ Begin.js
@@ -21,26 +21,48 @@ eraseButton.addEventListener('click', () => {
 });
 
 // 鼠标按下事件
-canvas.addEventListener('mousedown', (e) => {
-    if (drawing) {
-        ctx.beginPath();
-        ctx.moveTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
-        canvas.addEventListener('mousemove', draw);
-    }
-});
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('touchstart', startDrawing);
 
 // 鼠标移动事件
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('touchmove', draw);
+
+// 鼠标释放事件
+canvas.addEventListener('mouseup', stopDrawing);
+canvas.addEventListener('touchend', stopDrawing);
+
+// 开始绘制
+function startDrawing(e) {
+    if (drawing) {
+        e.preventDefault();
+        ctx.beginPath();
+        if (e.type === 'mousedown') {
+            ctx.moveTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
+        } else if (e.type === 'touchstart') {
+            ctx.moveTo(e.touches[0].clientX - canvas.getBoundingClientRect().left, e.touches[0].clientY - canvas.getBoundingClientRect().top);
+        }
+    }
+}
+
+// 绘制
 function draw(e) {
     if (drawing) {
-        ctx.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
+        e.preventDefault();
+        if (e.type === 'mousemove') {
+            ctx.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
+        } else if (e.type === 'touchmove') {
+            ctx.lineTo(e.touches[0].clientX - canvas.getBoundingClientRect().left, e.touches[0].clientY - canvas.getBoundingClientRect().top);
+        }
         ctx.stroke();
     }
 }
 
-// 鼠标释放事件
-canvas.addEventListener('mouseup', () => {
+// 停止绘制
+function stopDrawing() {
     canvas.removeEventListener('mousemove', draw);
-});
+    canvas.removeEventListener('touchmove', draw);
+}
 
 // 清空画布
 function clearCanvas() {
